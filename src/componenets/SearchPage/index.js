6202f6ab0f3637cf632827278ebf386b92b36485
Peter <petershotbox@gmail.com>
2023-02-14 02:56:37 +0000

    added styles for small screens as well as gradients to tiles

--- src/componenets/SearchPage/index.js
+++ src/componenets/SearchPage/index.js
@@ -4,6 +4,8 @@ export default function SearchPage({onEnter}){
     return(
         <div className="SearchPage">
             <input type="text" placeholder="Search" onKeyDown={(e)=>{ onEnter(e) }}/>
+            <p>( vvv There will be the popular films here vvv )</p>
+            <p>( In the mean time, have a go searching... )</p>
         </div>
     )
 }
